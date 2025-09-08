a7276561b9abad6070d8fcf0eb3525e96394faea
Robin Rollin <10898753+Sr0bin@users.noreply.github.com>
2025-09-08 18:09:29 +0200

    Working heredoc I think?

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -6,23 +6,23 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 18:02:54 by rorollin          #+#    #+#             */
-/*   Updated: 2025/09/04 15:28:21 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/09/08 17:27:32 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_var	*var_search(t_env *env, const char *key)
+t_var	*var_search(t_env *env, const char *key_to_search)
 {
 	t_env	*crnt_var;
 	ptrdiff_t	len;
 	
 
 	crnt_var = env;
-	len = var_expand_end(key) - key;
+	len = var_expand_end(key_to_search) - key_to_search;
 	while (crnt_var != NULL)
 	{
-		if (ft_strncmp(((t_var *) (crnt_var->content))->key, key, (size_t) len) == 0)
+		if (ft_strncmp(((t_var *) (crnt_var->content))->key, key_to_search, (size_t) len) == 0)
 			return (crnt_var->content);
 		crnt_var = crnt_var->next;
 	}
--- src/heredoc/heredoc.c
+++ src/heredoc/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 17:45:01 by rorollin          #+#    #+#             */
-/*   Updated: 2025/09/05 16:11:37 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/09/08 18:06:22 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,30 @@ static char	*temp_folder_path(int fd)
 	return (temp_path);
 }
 
-t_redir	*heredoc_create_fd()
+t_redir	*heredoc_create_fd(t_token_list *eof_token)
 {
 	t_redir	*redir;
 	char	*temp_path;
+	char	*eof_string;
 	int	w_fd;
 	int	r_fd;
 
+	if (eof_token == NULL | eof_token->next == NULL)
+	{
+		ft_error("Heredoc failed", 127);
+		return (NULL); //TODO: Syntqx error
+	}
 	redir = ft_calloc(1, sizeof(t_redir));
 	//TODO: protection
 	redir->type = REDIR_HEREDOC;
+	eof_string = lst_to_tkn(eof_token->next)->content;
 	w_fd = open("/tmp", O_TMPFILE | O_WRONLY, 0600);
 	if (w_fd < 0)
 	{
 		/*ft_error(exec_data_context(NULL, READ_EXEC_DATA), "Can't open temp file for Heredoc\n", 127);*/
 		return (NULL);
 	}
-	write(w_fd, "test decriture", 20);
+	prompt_heredoc(w_fd, eof_string);
 	temp_path = temp_folder_path(w_fd);
 	//TODO: Error check
 	r_fd = open(temp_path, O_RDONLY);
