514641c94f17c267e3461210820424050a2a828a
Robin Rollin <10898753+Sr0bin@users.noreply.github.com>
2025-09-08 18:51:21 +0200

    Reworked token_list_clean

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -6,18 +6,17 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 18:02:54 by rorollin          #+#    #+#             */
-/*   Updated: 2025/09/08 17:27:32 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/09/08 18:31:51 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_var	*var_search(t_env *env, const char *key_to_search)
+t_var	*var_search(t_env *env, char *key_to_search)
 {
 	t_env	*crnt_var;
 	ptrdiff_t	len;
 	
-
 	crnt_var = env;
 	len = var_expand_end(key_to_search) - key_to_search;
 	while (crnt_var != NULL)
