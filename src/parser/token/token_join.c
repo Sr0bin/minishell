5c25944f7e01b3db973eafa05b9c6d3e113a49c8
Robin Rollin <10898753+Sr0bin@users.noreply.github.com>
2025-09-22 13:23:19 +0200

    Fixed the joinig quote bug, and the expand is now recursive

--- src/parser/token/token_join.c
+++ src/parser/token/token_join.c
@@ -6,26 +6,28 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 13:03:23 by rorollin          #+#    #+#             */
-/*   Updated: 2025/09/22 13:03:59 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/09/22 13:16:22 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_token_list	*token_join(t_token_list *first, t_token_list *deleted)
+t_token_list	*token_join(t_token_list *first)
 {
 	t_token	*frst_tkn;
-	t_token	*del_tkn;
+	t_token	*scnd_tkn;
 	char	*ret;
 
 	frst_tkn = lst_to_tkn(first);
-	del_tkn = lst_to_tkn(deleted);
-	if (del_tkn->to_join != 1)
+	scnd_tkn = lst_to_tkn(first->next);
+	if (scnd_tkn->to_join == 0)
 		frst_tkn->to_join = 0;
-	token_clean_quote(del_tkn);
-	ret = ft_strcat(&frst_tkn->content, del_tkn->content);
+	token_clean_quote(scnd_tkn);
+	ret = ft_strcat(&frst_tkn->content, scnd_tkn->content);
 	if (ret == NULL)
 		return (NULL);
 	ft_lstpop(&first, (void *) free);
+	if (frst_tkn->to_join == 1)
+		return (token_join(first));
 	return (first);
 }
--- src/parser/token/token_list_clean.c
+++ src/parser/token/token_list_clean.c
@@ -6,7 +6,7 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 17:22:58 by rorollin          #+#    #+#             */
-/*   Updated: 2025/09/22 12:46:44 by rorollin         ###   ########.fr       */
+/*   Updated: 2025/09/22 13:20:59 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,19 @@ static t_token_list	*token_clean(t_token_list *tkn_lst)
 	tkn = lst_to_tkn(tkn_lst);
 	tkn_next = lst_to_tkn(tkn_lst->next);
 	assign_token_type(tkn);
-	ret = token_expand(tkn);
-	if (ret == NULL)
-		return (NULL);
+	while (ft_strchr(tkn->content, '$') != 0)
+	{
+		ret = token_expand(tkn);
+		if (ret == NULL)
+			return (NULL);
+	}
 	token_clean_quote(tkn);
 	if (tkn_next != NULL && tkn->to_join == 1)
 	{
-		ret = token_join(tkn_lst, tkn_lst->next);
+		ret = token_join(tkn_lst);
 		if (tkn->content == NULL || ret == NULL)
 			return (NULL);
-		return (tkn_lst->next);
+		return (ret);
 	}
 	return (tkn_lst);
 }
@@ -82,7 +85,6 @@ t_token_list	*token_list_clean(t_token_list **tkn_lst)
 {
 	t_token_list	*iter_prev;
 	t_token_list	*iter;
-	t_token			*crnt_token;
 	void			*ret;
 
 	if (*tkn_lst == NULL)
@@ -97,23 +99,5 @@ t_token_list	*token_list_clean(t_token_list **tkn_lst)
 		iter = iter_prev->next;
 	}
 	ret = token_list_loop(&iter, iter_prev);
-	while (iter != NULL)
-	{
-		crnt_token = iter->content;
-		if (token_list_empty(crnt_token, &iter_prev))
-			iter = iter_prev->next;
-		else
-		{
-			ret = token_clean(iter);
-			if (ret == iter->next)
-				continue ;
-			if (ret == NULL)
-				return (token_list_destroy(tkn_lst));
-			if (iter_prev->next == NULL)
-				break ;
-			iter_prev = iter;
-			iter = iter->next;
-		}
-	}
 	return (*tkn_lst);
 }
